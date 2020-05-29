855f8ee817ccd97f7121b2ccd379eea3e2606f71
William Washington <will.da.coder@gmail.com>
2020-05-28 22:08:20 -0400

    completed 1st kata

---
+++ main.js
@@ -0,0 +1,20 @@
+// Katas 1 ADD
+
+function add(x, y) {
+    return 2 + 4
+}
+
+console.log(add(2, 4))
+
+
+// Katas 2 Multiply
+
+function add(x, y) {
+    return 6 + 4
+}
+
+
+console.log(add(6, 4))
+
+
+
