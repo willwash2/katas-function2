c030ce5610c012a9d7f62a10fa958b292d123553
William Washington <will.da.coder@gmail.com>
2020-07-16 10:20:45 -0400

    Finished assesment

--- main.js
+++ main.js
@@ -1,20 +1,44 @@
 // Katas 1 ADD
 
 function add(x, y) {
-    return 2 + 4
+    return x + y
 }
-
-console.log(add(2, 4))
-
+console.log(add(2, 5))
 
 // Katas 2 Multiply
 
-function add(x, y) {
-    return 6 + 4
+function multiply(x, y) {
+    let total = 0
+    for (let i = 0; i < y; i++) {
+        total = add(x, total)
+    }
+    return total
 }
+console.log(multiply(7, 6))
 
+// Katas 3 power
 
-console.log(add(6, 4))
+function power(x, y) {
+    let total = 1
+    for (let i = 0; i < y; i++) {
+        total = multiply(x, total)
+    }
+    return total
+}
+console.log(power(2, 9))
+
+// Katas 4 factorial
+
+function factorial(x) {
+    let total = 1
+    for (let i = x; i > 1; i--) {
+        total = multiply(i, total)
+
+    }
+    return total
+}
+console.log(factorial(5))
+
 
 
 
